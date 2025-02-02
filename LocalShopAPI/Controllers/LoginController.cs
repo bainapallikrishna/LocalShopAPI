c9f35ec6dfa525f871733d84d75b29a8e187df37
bainapalli krishna <bainapallikrishna201@gmail.com>
2025-02-02 11:51:51 +0530

    Add project files.

---
+++ LocalShopAPI/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocalShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpGet(Name = "Login")]
+        public Boolean Login(string username, string password)
+        {
+            if(username=="krishna" && password=="krishna")
+            return true;
+            else return false;
+        }
+    }
+}
---
+++ Model/Register.cs
@@ -0,0 +1,12 @@
+﻿namespace Model
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string EmployeeName { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+    }
+}
